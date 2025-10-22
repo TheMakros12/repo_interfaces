ead90fb36cdb6fa3bb3fc7f81e9f310692676613
TheMakros12 <marcosnbx@gmail.com>
2025-10-22 13:57:23 +0000

    Subida de archivos de clase 22/10/25

--- 01_Intro_Qt/03_dialogo_transferencia/dtransferencia.h
+++ 01_Intro_Qt/03_dialogo_transferencia/dtransferencia.h
@@ -1,9 +1,17 @@
 #include <QDialog>
+#include <QLineEdit>
 
 class DTransferencia : public QDialog {
+Q_OBJECT
 
 public:
     DTransferencia (QDialog *parent = NULL);
     int procesar(int);
+    QPushButton *btnAceptar;
+    QLineEdit *leCantidad, *leDestino;
+
+public slots:
+    void activarBtn(const QString &);
+    void rechazarDialogo();
 
 };
